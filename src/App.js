5d3ebf736e15dce99e95f888b9def0898fa7d6c3
Fran <franciscorualesp15@gmail.com>
2023-02-26 19:03:35 -0500

    proyecto estructurado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Brand, Cta, NavBar } from './components';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        React
-      </h1>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
